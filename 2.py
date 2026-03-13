1fbddef4edd9366d3df277e5755fedd53873bfc0
Manuel Ytuza <alejo.ytuza@gmail.com>
2026-03-12 22:11:57 -0500

    fin: class_2_python

---
+++ 2.py
@@ -0,0 +1,49 @@
+"""
+NOTA DE PARTICPACION EJEMPLO DE UNA EMPRESA GRANDE Y SEGUN LO INVESTIGADO DONDE USAN EL PYTHON (CLOUD...)
+“Netflix usa Python para analizar datos de más de 260 millones de usuarios y generar recomendaciones automáticas. Además, funciona en cloud computing, lo que permite manejar grandes volúmenes de información y mejorar la experiencia del usuario.”
+“Instagram usa Python para manejar una plataforma con más de 2 mil millones de usuarios. Python ayuda a analizar datos, recomendar contenido y gestionar servicios en la nube.”
+--------------------------------
+crear un boot que ingrese a un app o pagina como usuario
+
+def impar_par (number):
+   if number % 2 == 0 : 
+      print ("es par")
+   else :
+      print("es inpar")
+"""
+#que es un frain 
+#arrays
+#list_1 = (1,2,3)
+#for n in list_1:
+   # print (n)
+
+#recorrer del 1 al 20
+#si el numero es divisible entre 3, saltalo continue
+#si es el numero es mayor a 15 break el bucle
+#imprime solo los numeros validos
+
+for i in range(1,20):#recorrer del 1 al 20
+  if(i%3==0):#si el numero es divisible entre 3, saltalo continue
+    continue
+  if (i > 15):#si es el numero es mayor a 15 break el bucle
+    break
+  print(i ,"es valido")#imprime solo los numeros validos
+    
+#un bucle siempre termina su variable contadora o i en uno mas de la condicion por ello se rompe el bucle 
+#SQL y ORACLE
+#Data Frein 
+
+#los conecptos son iguales pero la sintaxis varia en cada lenguaje
+
+#coleciones manejo de archivos y de errores
+
+
+
+
+
+
+
+#Participacion: buscar un ejercicio del for para un array, vector, fichero 
+#subir tareas antes de siguiente clase
+#ver tutorial de tareas en teams envio
+#revisar enunciado 04 de teams
