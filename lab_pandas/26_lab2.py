5c6828de61bd136b54554fe2c1ba4e69dac17a6f
Manuel Ytuza <alejo.ytuza@gmail.com>
2026-03-27 15:39:45 -0500

    fin_27

--- lab_pandas/26_lab2.py
+++ lab_pandas/26_lab2.py
@@ -7,7 +7,7 @@ Original file is located at
     https://colab.research.google.com/drive/1J5yHIJEAO4nh-tCAdwH7zzV0b8ZWTnqT
 """
 
-import pandas as pd
+import pandas as pd 
 
 peru_df = pd.read_excel('peru_df.xlsx')
 #peru_df = peru_df.set_index('dep')
